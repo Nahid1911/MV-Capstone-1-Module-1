10dcd80df02711a70b68778eaa19a3f51f072b76
Nahid1911 <nahid.raihan>
2023-04-19 10:28:45 +0600

    add about.html

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-let button = document.getElementById("hambargerIcon");
-button.addEventListener('click', function(){
 
-    let diVMobileMenue = document.createElement('dev');
-    diVMobileMenue.id = 'mobileMenue';
-    diVMobileMenue.innerHTML = `
-    <div class='mobileMenueDiv'>
-        <img id='cancelBtn' class='cancelBtn' src='assets/Repeat_background.svg'>
-        <ul class='mobileMenueUl'>
-            <li>About</li>
-            <li>Program</li>
-            <li>Join</li>
-            <li>Sponsor</li>
-            <li>News</li>
-        </ul>
-    </div>
-    `
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("hambargerIcon");
+var span = document.getElementsByClassName("cancelBtn")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
     let target1 = document.getElementById("hambargerIcon");
-    target1.classList.add('displayNone')
-    let target = document.getElementById('mainContainer');
-    target.insertBefore(diVMobileMenue, target.firstChild);
-});
+    target1.classList.remove('displayNone')
+}
 
-let cancelButton = document.getElementById('cancelBtn');
-console.log(id);
-cancelButton.addEventListener('click', function (){
-    if(document.getElementById('mobileMenue').classList.contains('displayNone') === true){
-        document.getElementById('mobileMenue').classList.remove('displayNone');
-    }
-})
