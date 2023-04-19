07b70f0ce2d75cda253b2088a7b8c5c399494200
Nahid1911 <nahid.raihan>
2023-04-19 20:00:41 +0600

    add about page mobile and desktop

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const modal = document.getElementById('myModal');
 const btn = document.getElementById('hambargerIcon');
 const span = document.getElementsByClassName('cancelBtn')[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = 'block';
-}
+};
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = 'none';
-  let target1 = document.getElementById('hambargerIcon');
-  target1.classList.remove('displayNone');
 };
