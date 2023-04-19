2102ccd28489e32440b92c95bf627447677e7aec
Nahid1911 <nahid.raihan>
2023-04-19 11:42:52 +0600

    fix ESLint and Stylelint indentation

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("hambargerIcon");
-var span = document.getElementsByClassName("cancelBtn")[0];
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('hambargerIcon');
+const span = document.getElementsByClassName('cancelBtn')[0];
 
 btn.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = 'block';
 }
 
 span.onclick = function() {
-    modal.style.display = "none";
-    let target1 = document.getElementById("hambargerIcon");
-    target1.classList.remove('displayNone')
-}
-
+  modal.style.display = 'none';
+  let target1 = document.getElementById('hambargerIcon');
+  target1.classList.remove('displayNone');
+};
